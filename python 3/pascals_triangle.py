ed89f2ea9bf92f23b9302e5bff5ab2bb460a030f
Juntao Dong <juntaodong@gmail.com>
2019-04-06 21:19:52 -0400

    update

--- python 3/pascals_triangle.py
+++ python 3/pascals_triangle.py
@@ -1,2 +1,11 @@
 from typing import List
 
+class Solution:
+	def generate(self, numRows: int) -> List[List[int]]:
+		if numRows==0:
+			return []
+		S=[[1]]
+		for i in range(numRows-1):
+			A=S[-1]
+			S.append([sum(x) for x in zip(A+[0], [0]+A)])
+		return S
